a7014f1f53a088abeb20c969f835c19baee6c693
mawguai <mawguai@gmail.com>
2018-03-01 02:06:58 +0100

    Init project

---
+++ hascode/src/main/java/fr/xebia/hashcode/HashCodeApp.java
@@ -0,0 +1,9 @@
+package fr.xebia.hashcode;
+
+public class HashCodeApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!"); // Display the string.
+    }
+
+}
